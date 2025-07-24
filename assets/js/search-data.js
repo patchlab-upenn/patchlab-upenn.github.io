1139ab4b1260fbd4834e72b66c1b96ecc0cccd43
Pulkit-Khandelwal <kpulkit95@gmail.com>
2025-07-24 03:38:00 +0000

    Deploying to gh-pages from @ patchlab-upenn/patchlab-upenn.github.io@698ac87ff2758e98efd0a5f6631c002f43ef4649 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-people",
           title: "people",
-          description: "members of the lab or group",
+          description: "lab members",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
@@ -43,13 +43,6 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
-          },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
           title: "teaching",
